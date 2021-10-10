19026acb14f739d3d6524a0cffd38149b4bc267a
rizqi.ekoputris <rizqi.ekoputris@pinhome.id>
2021-10-10 11:23:17 +0700

    debugging

--- modules/transformer_decoder.py
+++ modules/transformer_decoder.py
@@ -86,6 +86,7 @@ class Decoder(nn.Module):
               relu_dropout)
 
     self.proj_flag = False
+    print(embedding_size,hidden_size, "HOHO")
     if(embedding_size == hidden_size):
         self.embedding_proj = nn.Linear(embedding_size, hidden_size, bias=False)
         self.proj_flag = True
