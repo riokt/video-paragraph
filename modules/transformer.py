6a1602f66b02b2a8bb7f59b6505aa2a62ffc4fcf
Rizqi Okta <u7033006@anu.edu.au>
2021-10-19 20:24:29 +0700

    fix:debugging-trgmask

--- modules/transformer.py
+++ modules/transformer.py
@@ -59,7 +59,7 @@ class Transformer(nn.Module):
       print(trg[:,i-1].unsqueeze(1).shape, trg_mask[:,i-1,:i].unsqueeze(1).shape)
       
     for i in range(1, trg.size(1)+1):
-      word, attn, _ = self.decoder(trg[:,i-1].unsqueeze(1), memory_bank, src_mask, trg_mask[:,i-1,i].unsqueeze(1), step=i)
+      word, attn, _ = self.decoder(trg[:,i-1].unsqueeze(1), memory_bank, src_mask, trg_mask[:,i-1,i].unsqueeze(1), step=i-1)
       d_output.append(word[:,-1])
       attn_weights.append(attn[:,:,-1].mean(dim=1))
       memory_bank, add_state = self.update_memory(memory_bank, add_state, e_outputs, attn_weights[-20:], d_output[-20:])
